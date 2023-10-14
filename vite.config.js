6df02602659a70e842fd7c7cf931a9b86ec0ddbd
Ryan DeBerardinis <rdeber@gmail.com>
2023-10-14 11:28:11 -0400

    Add image optimization process

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from "vite";
 import postcssNesting from "postcss-nesting";
 import autoprefixer from "autoprefixer";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 85,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+    }),
+  ],
   css: {
     postcss: {
-      plugins: [postcssNesting, autoprefixer]
+      plugins: [
+        postcssNesting,
+        autoprefixer
+      ]
     }
   }
 });
