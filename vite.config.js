4ee560a6ba4cac72347bcd3b1f332599ebe12a61
Ryan DeBerardinis <rdeber@gmail.com>
2023-10-14 12:22:47 -0400

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,36 @@
 import { defineConfig } from "vite";
 import postcssNesting from "postcss-nesting";
 import autoprefixer from "autoprefixer";
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   plugins: [
+    // Image optimizer settings
     ViteImageOptimizer({
       png: {
-        // https://sharp.pixelplumbing.com/api-output#png
         quality: 85,
       },
       jpeg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
         quality: 90,
       },
       jpg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
         quality: 90,
       },
     }),
   ],
   css: {
     postcss: {
-      plugins: [
-        postcssNesting,
-        autoprefixer
-      ]
-    }
-  }
+      plugins: [postcssNesting, autoprefixer],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Preserve file names in production, otherwise Vite will use hashes.
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
